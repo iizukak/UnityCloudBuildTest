c03e7490f696a00eab56456122616f4096ffe6b9
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2018-09-29 13:27:51 +0900

    Add First Test

---
+++ Assets/Scripts/MyAdder.cs
@@ -0,0 +1,7 @@
+﻿public static class MyAdder
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+}
---
+++ Assets/Tests/MyAdderTests.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MyAdderTests
+{
+    [Test]
+    public void MyAdderTestSimple()
+    {
+        Assert.AreEqual(MyAdder.Add(1, 2), 3);
+    }
+}
